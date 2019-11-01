18b7effe8491610a85a5da4e5ca6e98efdfcf9bf
bamfranceschi <anna.franceschi@gmail.com>
2019-11-01 10:55:02 -0500

    createCard function complete for cards.js challenge

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,47 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// axios
+//   .get("https://lambda-times-backend.herokuapp.com/articles")
+//   .then(response => {
+//     console.log(response);
+//   });
+
+//response.data.articles  within here, we have 5 arrays filled with objects. Each array corresponds with a topic.
+//articles is itself an object
+
+//i think we'll be doing something like this? response.data.articles.bootstrap, as an example. Bootstrap is an array with 3 articles, each article object has a headline, authorPhoto and authorName
+
+//should have a total of 15 articles
+
+//i think we want to do Object.entries. so first we'd do const articleEntries = Object.entries(articles); and then we can do articleEntries.forEach
+
+// createCard function
+
+function createCard(headline, authorPhoto, authorName) {
+  const newCard = document.createElement("div");
+  newCard.classList.add("card");
+
+  const cardHeadline = document.createElement("div");
+  cardHeadline.textContent = headline;
+  newCard.appendChild(cardHeadline);
+
+  const cardAuthor = document.createElement("div");
+  cardAuthor.classList.add("author");
+  newCard.appendChild(cardAuthor);
+
+  const cardImgDiv = document.createElement("div");
+  cardImgDiv.classList.add("img-container");
+  cardAuthor.appendChild(cardImgDiv);
+
+  const authorImg = document.createElement("img");
+  authorImg.src = authorPhoto;
+  cardImgDiv.appendChild(authorImg);
+
+  const cardAuthorName = document.createElement("span");
+  cardAuthorName.textContent = authorName;
+  cardAuthor.appendChild(cardAuthorName);
+
+  return newCard;
+}
