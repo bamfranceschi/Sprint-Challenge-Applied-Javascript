d42cabccf4eec3143d1fccc1b010748a8935e6b8
bamfranceschi <anna.franceschi@gmail.com>
2019-11-01 12:37:48 -0500

    attempted stretch goal, did not complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,50 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+let img1 = new Image();
+img1.src = "./assets/carousel/mountains.jpeg";
+let img2 = new Image();
+img2.src = "./assets/carousel/computer.jpeg";
+let img3 = new Image();
+img3.src = "./assets/carousel/trees.jpeg";
+let img4 = new Image();
+img4.src = "./assets/carousel/turntable.jpeg";
+
+const currentIndex = [img1, img2, img3, img4];
+
+//Carousel Component
+
+function createCarousel(img1, img2, img3, img4) {
+  const newCarousel = document.createElement("div");
+  newCarousel.classList.add("carousel");
+
+  const leftBtn = document.createElement("div");
+  leftBtn.classList.add("left-button");
+  leftBtn.addEventListener("click", () => {
+    leftBtn.animate();
+  });
+  newCarousel.appendChild(leftBtn);
+
+  const firstImg = document.createElement("img");
+  firstImg.src = img1;
+  newCarousel.appendChild(firstImg);
+
+  const secondImg = document.createElement("img");
+  secondImg.src = img2;
+  newCarousel.appendChild(secondImg);
+
+  const thirdImg = document.createElement("img");
+  thirdImg.src = img3;
+  newCarousel.appendChild(thirdImg);
+
+  const fourthImg = document.createElement("img");
+  fourthImg.src = img4;
+  newCarousel.appendChild(fourthImg);
+
+  const rightBtn = document.createElement("div");
+  rightBtn.classList.add("right-button");
+  newCarousel.appendChild(rightBtn);
+
+  return newCarousel;
+}
